6b176106e3d38b2bc4906e637a46d6b2c66108b7
JEONGSUJI <wtw9611@gmail.com>
2020-01-31 22:05:12 +0900

    Split list according to bookshelf

--- src/BookLists.js
+++ src/BookLists.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
 class BookList extends Component{ 
+  
   render(){
     const { books } = this.props;
-    console.log('원하는 값', books );
+    const currentlyReading = books.filter((book)=>(book.shelf==='currentlyReading'));
+    const wantToRead = books.filter((book)=>(book.shelf==='wantToRead'));
+    const read = books.filter((book)=>(book.shelf==='read'));;
 
     return(
       <div className="list-books-content">
@@ -12,10 +15,11 @@ class BookList extends Component{
             <h2 className="bookshelf-title">Currently Reading</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                <li>
-                  <div className="book">
+                {currentlyReading.map((book)=>(
+                  <li key={book.id}>
+                    <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks['smallThumbnail']})`}}></div>
                       <div className="book-shelf-changer">
                         <select>
                           <option value="move" disabled>Move to...</option>
@@ -26,10 +30,12 @@ class BookList extends Component{
                         </select>
                       </div>
                     </div>
-                    <div className="book-title">To Kill a Mockingbird</div>
-                    <div className="book-authors">Harper Lee</div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                    <div className="book-authors">{book.shelf}</div>
                   </div>
                 </li>
+                ))}
               </ol>
             </div>
           </div>
@@ -37,10 +43,11 @@ class BookList extends Component{
             <h2 className="bookshelf-title">Want to Read</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                <li>
-                  <div className="book">
+                {wantToRead.map((book)=>(
+                  <li key={book.id}>
+                    <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks['smallThumbnail']})`}}></div>
                       <div className="book-shelf-changer">
                         <select>
                           <option value="move" disabled>Move to...</option>
@@ -51,10 +58,12 @@ class BookList extends Component{
                         </select>
                       </div>
                     </div>
-                    <div className="book-title">1776</div>
-                    <div className="book-authors">David McCullough</div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                    <div className="book-authors">{book.shelf}</div>
                   </div>
                 </li>
+                ))}
               </ol>
             </div>
           </div>
@@ -62,10 +71,11 @@ class BookList extends Component{
             <h2 className="bookshelf-title">Read</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                <li>
-                  <div className="book">
+                {read.map((book)=>(
+                  <li key={book.id}>
+                    <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks['smallThumbnail']})`}}></div>
                       <div className="book-shelf-changer">
                         <select>
                           <option value="move" disabled>Move to...</option>
@@ -76,10 +86,12 @@ class BookList extends Component{
                         </select>
                       </div>
                     </div>
-                    <div className="book-title">The Hobbit</div>
-                    <div className="book-authors">J.R.R. Tolkien</div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                    <div className="book-authors">{book.shelf}</div>
                   </div>
                 </li>
+                ))}
               </ol>
             </div>
           </div>
